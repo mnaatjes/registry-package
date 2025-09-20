8399ae459c6826a8a8be9b869a9971c5247b8f33
mnaatjes <michael.naatjes87@gmail.com>
2025-09-20 16:13:07 +0000

    added comments to ServiceRegistry

--- src/ServiceRegistry.php
+++ src/ServiceRegistry.php
@@ -7,14 +7,33 @@
     use mnaatjes\Registry\Components\RegistryMetaData;
 
     class ServiceRegistry {
+        /**
+         * The instance of the registry
+         * @var ServiceRegistry
+         */
         private static ?ServiceRegistry $instance;
+
+        /**
+         * The root node of the registry
+         * @var RegistryNode
+         */
         private RegistryNode $root;
 
+        /**
+         * The constructor
+         * @access private
+         * @return void
+         */
         private function __construct() {
             // Create the root node
             $this->root = new RegistryNode();
         }
 
+        /**
+         * Get the instance of the registry
+         * @access public
+         * @return ServiceRegistry
+         */
         public static function getInstance(): ServiceRegistry {
             // If the instance has not been created yet, create it
             if (!isset(self::$instance)) {
